280e9a39d758153111e9ddd9920f462d590dc28b
Bagus Eri Rizqiyan <baguseririzqiyan@gmail.com>
2020-08-26 11:52:46 +0700

    tugas3

---
+++ tugas-3/tugas3.js
@@ -0,0 +1,14 @@
+//soal 1
+
+var kataPertama = "saya ";
+var kataKedua = "senang ";
+var kataKetiga = "belajar ";
+var kataKeempat = "javascripts ";
+var upper = kataKeempat.toUpperCase(1);
+var gabung = kataPertama + kataKedua + kataKetiga + upper;
+console.log(gabung)
+// 
+// var kataPertama = "1";
+// var kataKedua = "2";
+// var kataKetiga = "4";
+// var kataKeempat = "5";
